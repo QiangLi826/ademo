a27b68a5d4fbfd2e67b1782a403adcc235d88552
dragonQiang <a625846910@163.com>
2020-06-17 09:59:22 +0800

    将处理完的图片归档输出

--- gui.py
+++ gui.py
@@ -7,6 +7,8 @@ import time
 
 import os
 
+from shutil import copyfile, copy
+
 frameT = Tk()
 frameT.geometry('500x200+400+200')
 frameT.title('选择需要输入处理的文件')
@@ -52,23 +54,46 @@ def count_images(dir_path):
 
     return count
 
-def process_images(dir_path, pb, total, count):
+def process_images(input_dir, output_dir, pb, total, count):
 
-    dir_files = os.listdir(dir_path)  # 得到该文件夹下所有的文件
+    dir_files = os.listdir(input_dir)  # 得到该文件夹下所有的文件
     for file in dir_files:
-        file_path = os.path.join(dir_path, file)  # 路径拼接成绝对路径
-        if os.path.isfile(file_path):  # 如果是文件，就打印这个文件路径
+        file_path = os.path.join(input_dir, file)  # 路径拼接成绝对路径
+        if os.path.isfile(file_path):
             if (file_path.lower().endswith(
                     ('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff'))):
-                count = count + 1
-                value = count * 100 / total
-                pb["value"] = value
-                frameT.update()
-                print(count)
-                time.sleep(1)
 
+                count = train(count, file, file_path, output_dir, pb, total)
+
+        sub_output_dir = os.path.join(output_dir, file)
         if os.path.isdir(file_path):  # 如果目录，就递归子目录
-            count_images(file_path,pb, total, count)
+            count = process_images(file_path, sub_output_dir, pb, total, count)
+
+    return count
+
+
+def train(count, file, file_path, output_dir, pb, total):
+    if not os.path.exists(os.path.join(output_dir, 'with_cave')):
+        os.makedirs(os.path.join(output_dir, 'with_cave'))
+    copy(file_path, os.path.join(output_dir, 'with_cave', file))
+    if not os.path.exists(os.path.join(output_dir, 'without_cave')):
+        os.makedirs(os.path.join(output_dir, 'without_cave'))
+    copy(file_path, os.path.join(output_dir, 'without_cave', file))
+    if not os.path.exists(os.path.join(output_dir, 'unknow')):
+        os.makedirs(os.path.join(output_dir, 'unknow'))
+    copy(file_path, os.path.join(output_dir, 'unknow', file))
+    count = fresh_processbar(count, pb, total)
+    return count
+
+
+def fresh_processbar(count, pb, total):
+    count = count + 1
+    value = count * 100 / total
+    pb["value"] = value
+    frameT.update()
+    print(count)
+    #time.sleep(1)
+    return count
 
 
 def start():
@@ -82,8 +107,10 @@ def start():
     if (os.path.exists(v1.get())):
         total = count_images(v1.get())
         print(total)
-        process_images(dir_path=v1.get(), pb=pb, total=total, count=0)
+        process_images(input_dir=v1.get(),output_dir=v2.get(), pb=pb, total=total, count=0)
 
+        #刷新总进度
+        fresh_processbar(total, pb, total)
     #
     #
     # for i in range(100):
